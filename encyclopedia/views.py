c2cae2445a219e17325ed30a60c6f33fbcc8da21
esthertseng1031 <jasmine40628@gmail.com>
2021-10-30 18:37:04 +0800

    Finished entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
+import markdown
 from django.shortcuts import render
-
 from . import util
 
 
@@ -10,16 +10,26 @@ def index(request):
 
 def entry(request, title):
     """
-    If an entry is requested that does not exist, the user should 
+    1. If an entry is requested that does not exist, the user should 
     be presented with an error page indicating that their requested
     page was not found.
+    
+    2. If the entry does exist, the user should be presented with a page
+    that displays the content of the entry.
     """
+    
     if not util.get_entry(title):
         return render(request, "encyclopedia/error.html", {
             "title": title.capitalize()
         })
-    return render(request, "encyclopedia/entry.html", {
-        "title": title,
-        "content": util.get_entry(title)
-    })
+    
+    else:
+        text = util.get_entry(title)
+        
+        # Convert the Markdown into HTML
+        html = markdown.markdown(text)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title.capitalize(),
+            "content": html
+        })
 
